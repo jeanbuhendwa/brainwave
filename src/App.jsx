1b8268570e19a2ebaf50178a5834c6ff23c6dd4b
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2024-05-31 12:44:21 +0300

    Update app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Button from "./components/Button";
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Button className="mt-10" href="#login">
           Something
