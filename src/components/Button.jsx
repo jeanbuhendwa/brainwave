f8a3dbe264e3b2aa1d74e9dc27d4b75d93151b12
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2024-05-31 12:44:36 +0300

    Update button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,29 @@
 import buttonSvg from "../assets/svg/ButtonSvg";
 
-const Button = ({ className, href, onClick, childreen, px, white }) => {
+const Button = ({ className, href, onClick, px, white, children }) => {
+  const classes = `button relative infline-flex items-center justify-center h-11 transistion-colors hover:text-color-1 ${
+    px || "px-7"
+  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
+
+  const spanClasses = `relative z-10`;
+
   const renderButton = () => (
-    <button>
-      <span>{childreen}</span>
+    <button className={classes}>
+      <span className={spanClasses}>{children}</span>
+
       {buttonSvg(white)}
     </button>
   );
-  return renderButton();
+
+  const renderLink = () => (
+    <button className={classes}>
+      <span className={spanClasses}>{children}</span>
+
+      {buttonSvg(white)}
+    </button>
+  );
+
+  return href ? renderLink() : renderButton();
 };
 
 export default Button;
